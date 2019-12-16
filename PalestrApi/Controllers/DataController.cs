70fdcfbfc38281ad970d5e4149a83ac37571519f
Roberto Fornaro <r.fornaro@studenti.local>
2019-12-16 11:51:20 +0100

    creadp

--- PalestrApi/Controllers/DataController.cs
+++ PalestrApi/Controllers/DataController.cs
@@ -9,6 +9,19 @@ namespace PalestrApi.Controllers
 {
     public class DataController : ApiController
     {
-        
+        public void test1() {
+            using (EntityModel db = new EntityModel())
+            {
+                var prenotazioni = (
+                    from p in db.Prenotazione
+                    join c in db.Clienti
+                    on p.IdCliente equals c.Id
+                    join pa in db.Palestre
+                    on p.IdPalestra equals pa.Id
+                    select p
+                   ).ToList();
+            }
+
+        }
     }
 }
---
+++ PalestrApi/Controllers/PalestresController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PalestrApi;
+
+namespace PalestrApi.Controllers
+{
+    public class PalestresController : ApiController
+    {
+        private EntityModel db = new EntityModel();
+
+        // GET: api/Palestres
+        public IQueryable<Palestre> GetPalestre()
+        {
+            return db.Palestre;
+        }
+
+        // GET: api/Palestres/5
+        [ResponseType(typeof(Palestre))]
+        public IHttpActionResult GetPalestre(int id)
+        {
+            Palestre palestre = db.Palestre.Find(id);
+            if (palestre == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(palestre);
+        }
+
+        // PUT: api/Palestres/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPalestre(int id, Palestre palestre)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != palestre.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(palestre).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PalestreExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Palestres
+        [ResponseType(typeof(Palestre))]
+        public IHttpActionResult PostPalestre(Palestre palestre)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Palestre.Add(palestre);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = palestre.Id }, palestre);
+        }
+
+        // DELETE: api/Palestres/5
+        [ResponseType(typeof(Palestre))]
+        public IHttpActionResult DeletePalestre(int id)
+        {
+            Palestre palestre = db.Palestre.Find(id);
+            if (palestre == null)
+            {
+                return NotFound();
+            }
+
+            db.Palestre.Remove(palestre);
+            db.SaveChanges();
+
+            return Ok(palestre);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PalestreExists(int id)
+        {
+            return db.Palestre.Count(e => e.Id == id) > 0;
+        }
+    }
+}
